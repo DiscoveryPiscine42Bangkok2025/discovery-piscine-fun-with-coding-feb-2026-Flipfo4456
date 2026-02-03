44dc69b562e63758e26f91098e3240683e1eee90
“Flipfo4456“ <sadbadmad3@gmail.com>
2026-02-04 01:04:13 +0700

    Done cell00

--- cell00/ex05/build.sh
+++ cell00/ex05/build.sh
@@ -1,7 +1,7 @@
 if [ $# -eq 0 ]; then #กลับไปดู argv.sh
     echo "No arguments supplied"
 else
-    for arg in "$@" #$@ = กี่ตัวกะดั้ย arg ก้ฟีลๆ i (dummy)
+    for arg in "$@" # "$@" = กี่ตัวกะดั้ย arg ก้ฟีลๆ i (dummy)
     do
         mkdir "ex$arg" # ex+ชื่อ str ที่ได้มา
     done
